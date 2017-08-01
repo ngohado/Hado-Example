4a634342cb28a8911f024ecb14b659db12a9acdb
doannh <doannh@ominext.com>
2017-08-01 17:25:23 +0700

    config gradle and trying to get number of week from julian day

--- calendar/src/main/java/com/hado/calendar/MainActivity.kt
+++ calendar/src/main/java/com/hado/calendar/MainActivity.kt
@@ -2,11 +2,18 @@ package com.hado.calendar
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import butterknife.BindView
+import butterknife.ButterKnife
 
 class MainActivity : AppCompatActivity() {
+
+    @BindView(R.id.rv_calendar)
+    lateinit var rvCalendar: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
     }
 }
---
+++ calendar/src/main/java/com/hado/calendar/TimeUtils.kt
@@ -0,0 +1,13 @@
+package com.hado.calendar
+
+import java.util.*
+
+/**
+ * Created by DoanNH on 8/1/2017.
+ */
+object TimeUtils {
+    fun getWeekSinceJulianDay() {
+        val calendar = GregorianCalendar.getInstance()
+
+    }
+}
